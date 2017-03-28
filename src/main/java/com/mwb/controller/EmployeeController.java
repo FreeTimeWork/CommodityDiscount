aab125564deb03127cbe5d5f607aa3a679fb4bcc
mengweibo <mengweibo@shbj.com>
2017-03-28 19:16:23 +0800

    【孟卫波】修改build

--- src/main/java/com/mwb/controller/EmployeeController.java
+++ src/main/java/com/mwb/controller/EmployeeController.java
@@ -1,11 +1,14 @@
 package com.mwb.controller;
 
+import com.alibaba.fastjson.JSONObject;
+import com.mwb.dao.model.ContentType;
 import com.mwb.dao.model.Employee;
 import com.mwb.dao.model.Log;
 import com.mwb.service.api.IEmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -38,6 +41,14 @@ public class EmployeeController {
             session.setAttribute("admin", manager);
             return "redirect:/static/manager/index";
         }
+    }
+    @ResponseBody
+    @RequestMapping(value = "/login1", produces = ContentType.APPLICATION_JSON_UTF8)
+    public String  Login() {
+        Employee employee1 = new Employee();
+        employee1.setPassword("12");
+
+        return JSONObject.toJSONString(employee1);
     }
 
 }
