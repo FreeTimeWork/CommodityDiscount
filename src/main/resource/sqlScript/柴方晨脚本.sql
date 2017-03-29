e10ce6d2a997d94a0dbd7b6d8b59b909a68a641c
fangchen.chai <1127671368@qq.com>
2017-03-29 14:38:13 +0800

    【柴方晨】脚本完成

--- src/main/resource/sqlScript/柴方晨脚本.sql
+++ src/main/resource/sqlScript/柴方晨脚本.sql
@@ -1,7 +1,18 @@
 use coupon;
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 -- 用户状态表
+DROP TABLE IF EXISTS `t_employee_status`;
 CREATE TABLE `t_employee_status` (
   `id` int(11) unsigned NOT NULL,
   `code` char(16) DEFAULT NULL,
@@ -11,6 +22,7 @@ CREATE TABLE `t_employee_status` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- 岗位
+DROP TABLE IF EXISTS `t_position`;
 CREATE TABLE `t_position` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `code` char(64) NOT NULL,
@@ -21,6 +33,7 @@ CREATE TABLE `t_position` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- 小组
+DROP TABLE IF EXISTS `t_group`;
 CREATE TABLE `t_group` (
   `id` char(16) NOT NULL,
   `code` char(64) NOT NULL,
@@ -31,6 +44,7 @@ CREATE TABLE `t_group` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- 用户表
+DROP TABLE IF EXISTS `t_employee`;
 CREATE TABLE `t_employee` (
   `id` char(16) NOT NULL,
   `full_name` varchar(32) NOT NULL,
@@ -49,6 +63,7 @@ CREATE TABLE `t_employee` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- 权限
+DROP TABLE IF EXISTS `t_permission`;
 CREATE TABLE `t_permission` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `code` char(64) NOT NULL,
@@ -60,6 +75,7 @@ CREATE TABLE `t_permission` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- 岗位-权限表
+DROP TABLE IF EXISTS `t_position_permission`;
 CREATE TABLE `t_position_permission` (
   `position_id` int(10) unsigned NOT NULL,
   `permission_id` int(10) unsigned NOT NULL,
@@ -70,5 +86,11 @@ CREATE TABLE `t_position_permission` (
   CONSTRAINT `fk_position_permission_position` FOREIGN KEY (`position_id`) REFERENCES `t_position` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
