4d3f19d8c53394435c8864ee776ec97dcfb47e4d
mengweibo <mengweibo@shbj.com>
2017-04-07 13:07:56 +0800

    【孟卫波】js

--- src/main/java/com/mwb/controller/exception/ExceptionController.java
+++ src/main/java/com/mwb/controller/exception/ExceptionController.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.ModelAndView;
  * 异常控制
  * 跳转页面
  */
-@ControllerAdvice
+//@ControllerAdvice
 public class ExceptionController {
 	private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(ExceptionController.class);
 
