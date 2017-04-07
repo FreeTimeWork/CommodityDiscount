045e6cda3159b908d61a440a63cb8383fd33de51
fangchen.chai <1127671368@qq.com>
2017-04-07 11:41:58 +0800

    [fangchen] fix && excel ok

---
+++ src/main/java/com/mwb/controller/finance/api/FinanceExcelConstants.java
@@ -0,0 +1,25 @@
+package com.mwb.controller.finance.api;
+
+public class FinanceExcelConstants {
+
+	public static final String SHEET_TITLE = "财务报表";
+	
+	public static final String LABEL_RANK = "排行";
+	public static final String LABEL_EMPLOYEE = "业务员";
+	public static final String LABEL_SUBMIT_NUM = "提报数量";
+	public static final String LABEL_AVERAGE_DAILY = "提报率";
+	public static final String LABEL_REFUSE_RATE = "拒绝率";
+	public static final String LABEL_REFUSE_NUM = "拒绝数量";
+	public static final String LABEL_TWO_AUDIT_NUM = "待二审数量";
+	public static final String LABEL_PROMOTE_NUM = "推广中数量";
+	public static final String LABEL_END_APPROACH_NUM = "即将结束数量";
+	public static final String LABEL_END_NUM = "结束数量";
+	public static final String LABEL_PAY_WAIT_NUM = "代付款数量";
+	public static final String LABEL_PAY_RUN_NUM = "付款中数量";
+	public static final String LABEL_PAY_TRAILER_NUM = "拒绝付款数量";
+	public static final String LABEL_PAY_END_NUM = "已付款数量";
+	public static final String LABEL_GUEST_UNIT_PRICE = "客单价";
+	public static final String LABEL_ACTUAL_CHARGE_AMOUNT = "实收金额";
+	public static final String LABEL_SHOULD_CHARGE_AMOUNT = "应收金额";
+
+}
--- src/main/java/com/mwb/controller/util/ApplicationContextUtils.java
+++ src/main/java/com/mwb/controller/util/ApplicationContextUtils.java
@@ -1,10 +1,12 @@
 package com.mwb.controller.util;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.context.request.ServletWebRequest;
 
 /**
  * Created by fangchen.chai on 2017/4/3.
@@ -22,5 +24,9 @@ public class ApplicationContextUtils {
     public static HttpServletRequest getRequest() {
         ServletRequestAttributes attrs =(ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         return attrs.getRequest();
+    }
+
+    public static HttpServletResponse getResponse() {
+        return ((ServletWebRequest)RequestContextHolder.getRequestAttributes()).getResponse();
     }
 }
