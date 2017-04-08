1387a8ccbbf7a565356ffc9d8d599b394f99f9f2
xiaochai1027 <1127671368@qq.com>
2017-04-08 11:08:00 +0800

    [fangchen.chai] LOG 终于ok

--- src/main/java/com/mwb/controller/exception/ExceptionController.java
+++ src/main/java/com/mwb/controller/exception/ExceptionController.java
@@ -1,7 +1,6 @@
 package com.mwb.controller.exception;
 
-import org.apache.log4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.mwb.dao.model.comm.Log;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -12,13 +11,11 @@ import org.springframework.web.servlet.ModelAndView;
 //// TODO: 2017/4/7  
 //@ControllerAdvice
 public class ExceptionController {
-	private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(ExceptionController.class);
-	private static Logger log4jLog = Logger.getLogger(ExceptionController.class);
+	private static final Log LOG = Log.getLog(ExceptionController.class);
 	@ExceptionHandler({Exception.class})
 	public ModelAndView Exception(Exception ex){
 
-		LOGGER.error("出错了异常",ex);
-		log4jLog.error("log4j异常",ex);
+		LOG.error("出错了异常",ex);
 		ModelAndView mv = new ModelAndView("404");
 
 		mv.addObject("exception", ex);
